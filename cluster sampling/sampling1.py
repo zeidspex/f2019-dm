b193913395bb49dffbed08243277c9c4c8b1911e
nfetisov-usf <nfetisov@mail.usf.edu>
2019-10-30 13:41:18 -0400

    Simplified the code

--- cluster sampling/sampling1.py
+++ cluster sampling/sampling1.py
@@ -3,52 +3,38 @@ import math
 import numpy as np
 import scipy.special as sks
 
-# Number of samples selected from the cluster
-n = 3
 
-# Array for temporary storing coefficients
-ks = np.array([0] * 10)
+def csprob(ps, n, i=1, ks=np.array([]).astype('int')):
+    """
+    :param ps: distribution of classes in the cluster
+    :param n: sample size
+    :param i: iteration (used by recursion)
+    :param ks: array of combinations (used by recursion)
+    :return: probability that the majority class in the sample is the same as in the cluster
 
-# Variable for storing result
-prob = 0
+    Sample input:
+        ps = [0.35, 0.15, 0.15, 0.15, 0.15, 0.05, 0, 0, 0, 0]; n = 5
+    """
+    assert (math.isclose(sum(ps), 1) and len(ps) == 10 and n >= 1)
 
-# Distribution of classes in the cluster, or probabilities of selecting each class
-ps = [0.35, 0.15, 0.15, 0.15, 0.15, 0.05, 0, 0, 0, 0]
+    if i == 1:
+        rng = range(int(np.ceil(n / 10)), n + 1)
+    elif i < 10:
+        rng = range(0, min(ks[0], n - sum(ks[0:i])) + 1)
+    else:
+        rng = range((n - sum(ks[0:9])), min(ks[0], n - sum(ks[0:9])) + 1)
 
-# Check that probabilities add up to 1
-assert math.isclose(sum(ps), 1)
-
-for k1 in range(int(np.ceil(n / 10)), n + 1):
-    ks[0] = k1
-    for k2 in range(0, min(k1, n - sum(ks[0:1])) + 1):
-        ks[1] = k2
-        for k3 in range(0, min(k1, n - sum(ks[0:2])) + 1):
-            ks[2] = k3
-            for k4 in range(0, min(k1, n - sum(ks[0:3])) + 1):
-                ks[3] = k4
-                for k5 in range(0, min(k1, n - sum(ks[0:4])) + 1):
-                    ks[4] = k5
-                    for k6 in range(0, min(k1, n - sum(ks[0:5])) + 1):
-                        ks[5] = k6
-                        for k7 in range(0, min(k1, n - sum(ks[0:6])) + 1):
-                            ks[6] = k7
-                            for k8 in range(0, min(k1, n - sum(ks[0:7])) + 1):
-                                ks[7] = k8
-                                for k9 in range(0, min(k1, n - sum(ks[0:8])) + 1):
-                                    ks[8] = k9
-                                    for k10 in range((n - sum(ks[0:9])), min(k1, n - sum(ks[0:9])) + 1):
-                                        ks[9] = k10
-
-                                        # Verify that the total number of items matches n
-                                        # Also verify that the first class is majority class
-                                        assert sum(ks) == n
-                                        assert all([ks[0] >= k for k in ks[1:]])
-
-                                        prob_d = [
-                                            sks.comb(n - np.sum(ks[0:i]), ks[i]) * (ps[i] ** ks[i])
-                                            for i in range(10)
-                                        ]
-                                        prob += np.prod(prob_d) * (1 / sum(ks == ks[0]))
-
-
-print('Probability that cluster is labeled correctly: %.3f' % prob)
+    if i < 10:
+        return np.sum([csprob(ps, n, i + 1, np.concatenate((ks, np.array([k])))) for k in rng])
+    else:
+        return np.sum([
+            (lambda ks:
+                np.prod(
+                    [
+                        sks.comb(n - np.sum(ks[0:i]), ks[i]) * (ps[i] ** ks[i])
+                        for i in range(10)
+                    ]
+                ) * (1 / np.sum(ks == ks[0]))
+            )(np.concatenate((ks, np.array([k]))))
+            for k in rng
+        ])
