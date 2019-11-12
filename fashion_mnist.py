21e923461f36840bbb84d40a3ed1d94c1cb37ad4
Jackie <Jackie@jackies-mbp.home>
2019-11-11 22:39:42 -0500

    set up

---
+++ fashion_mnist.py
@@ -0,0 +1,25 @@
+######################################
+########## Import Functions ##########
+
+import re
+import h5py
+import json
+import random
+import tarfile
+import itertools
+import testing
+import clustering
+import visualization
+import classification
+import numpy as np
+import keras as ks
+import tensorflow as tf
+import sklearn.model_selection as model_selection
+
+
+#######################################
+#### Variable Initalizations ##########
+
+
+#######################################
+#######Data Preparing##################
