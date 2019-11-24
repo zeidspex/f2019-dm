5c9390c29797679b3ddc1ab299be22e1c484ed0b
Jackie <Jackie@jackies-mbp.home>
2019-11-24 17:29:46 -0500

    Clustering classifier

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -14,7 +14,7 @@ import numpy as np
 import keras as ks
 import tensorflow as tf
 import sklearn.model_selection as model_selection
-
+import warnings
 
 #######################################
 #### Variable Initalizations ##########
@@ -23,20 +23,26 @@ batch_size = 128
 num_classes=10
 seed=66
 val_size=0.2
+labeled_samples_per_class = 9
 
 ##Autoencoder variables
 e_model_path = 'data/model_FashionMnist.h5'
-e_epochs = 1
+e_epochs = 5
 e_patience = 40
 e_learn_rate = 0.0001
 
 ##Classification variables
 c_model_path = 'data/model_FashionMnist_classifier.h5'
-c_epochs = 1
+c_epochs = 5
 c_patience = 40
 c_learn_rate = 0.001
 
+np.seterr(divide='ignore', invalid='ignore')
+x = np.ones((1000, 1000)) * np.nan
 
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=RuntimeWarning)
+    foo = np.nanmean(x, axis=1)
 #######################################
 #######Data Preparing##################
 
@@ -62,11 +68,13 @@ unlabeled_train_images, unlabeled_val_images = model_selection.train_test_split(
 
 print("Train images are of size: {0}, \n Train labels are of size {1}\n".format(train_images.shape,train_labels.shape))
 print("Test images are of size: {0}, \n Test labels are of size {1}\n".format(test_images.shape,test_labels.shape))
+print(" unlabeledimages are of size: {0}, \n Train labels are of size {1}\n".format(unlabeled_train_images.shape, unlabeled_val_images.shape))
 
 # #######################################
 # ####### Encoder Model Creation#########
 
 e_model_layers = fashion_utils.get_fashion_encoder_model()
+
 # Create and build the model from layers, print model info
 encoder_model = ks.models.Sequential(e_model_layers)
 encoder_model.compile(
@@ -111,19 +119,57 @@ print(score)
 #######################################################
 #########Classifier from Autoencoder model############
 
-classifier_layers = fashion_utils.get_fashion_classifier_model(e_model_path, num_classes)
-classifier_model = ks.models.Sequential(classifier_layers)
+#Build Classifier
+classifier_model = fashion_utils.get_fashion_classifier_model(e_model_path, num_classes)
 classifier_model.compile(
     optimizer=ks.optimizers.Adam(lr=c_learn_rate),
     loss=ks.losses.categorical_crossentropy,
     metrics=['accuracy']
 )
 
-classifier_model.summary()
-#Train Model
+# classifier_model.summary()
 
-#Test Model
+# #Split labelled test set into test and validation
+# labeled_train_images, labeled_val_images, train_labels, val_labels = model_selection.train_test_split(
+#     train_images, train_labels, test_size=val_size, random_state=seed, stratify=train_labels
+# )
+#
+z_train = classifier_model.predict(train_images)
+centroids = np.array([
+    np.mean(
+        z_train[np.argwhere(train_labels == i)].reshape(-1, z_train.shape[1])[0:labeled_samples_per_class],
+        axis=0
+    )
+    for i in range(10)
+])
 
+#Train Classifier
+# classifier_model.fit(train_images, train_labels,
+#     # validation_data=(labeled_val_images, labeled_val_images),
+#     batch_size=batch_size,  shuffle='batch',
+#     epochs=c_epochs,
+#     verbose=1,
+#     callbacks=[
+#         ks.callbacks.ModelCheckpoint(
+#             filepath=c_model_path, monitor='val_loss',
+#             verbose=0, save_best_only=True, save_weights_only=False, mode='auto',
+#         ),
+#         ks.callbacks.EarlyStopping(
+#             monitor='val_loss', min_delta=1E-6, patience=c_patience,
+#             verbose=0, mode='auto'
+#         ),
+#         ks.callbacks.ReduceLROnPlateau(
+#             monitor='val_loss', factor=0.5, patience=10, min_delta=1E-6
+#         ),
+#     ]
+# )
+#
+# classifier_model.load_weights(c_model_path)
+#
+# #Test Model
+# new_score = model.evaluate(test_images, test_labels, verbose=0)
+# print('Test loss:', new_score[0])
+# print('Test accuracy:', new_score[1])
 
 
 #######################################
--- fashion_utils.py
+++ fashion_utils.py
@@ -11,14 +11,14 @@ import keras as ks
 import tensorflow as tf
 import sklearn.model_selection as model_selection
 
-
+encoding_size = 15
 
 def get_fashion_encoder_model():
 
     model_layers = [ks.layers.InputLayer(input_shape=(28, 28,1))]
 
     # Convolutional layers (5 x (Conv + Max Pool))
-    for n_filters in 64 * 2 ** np.array(range(2)):
+    for n_filters in 32 * 2 ** np.array(range(2)):
         model_layers.append(ks.layers.Conv2D(
             filters=n_filters, kernel_size=(3, 3),
             activation='relu', padding='same',
@@ -28,8 +28,9 @@ def get_fashion_encoder_model():
     # Dense layers
     model_layers += [
         ks.layers.Flatten(),
-        ks.layers.Dense(7 * 7 * 128),
-        ks.layers.Reshape((7, 7, 128))
+        ks.layers.Dense(392, activation='relu'),
+        ks.layers.Dense(196, activation='relu'),
+        ks.layers.Reshape((7, 7, 4))
     ]
 
     # Deconvolutional layers
@@ -44,27 +45,22 @@ def get_fashion_encoder_model():
 def get_fashion_classifier_model(model_path, num_classes):
 
     # Create and build the model from layers, print model info
-    # fashion_classifier = ks.models.Sequential()
-    fashion_class_model = [ks.layers.InputLayer(input_shape=(28, 28,1))]
+    fashion_class_model = ks.models.Sequential()
+    # fashion_class_model = [ks.layers.InputLayer(input_shape=(28, 28,1))]
 
     weights_model = ks.models.Sequential(get_fashion_encoder_model())
     weights_model.load_weights(model_path)
 
     for layer in weights_model.layers[:len(get_fashion_encoder_model())]:
         layer.trainable = False
-        fashion_class_model.append(layer)
+        fashion_class_model.add(layer)
 
-    # classifier_model.add(ks.layers.Flatten())
-    # classifier_model.add(ks.layers.Dense(196, activation='relu'))
-    # fashion_classifier.append(ks.layers.Dropout(0.2))
-    # fashion_classifier.append(ks.layers.Dense(40, activation='relu'))
-    # fashion_classifier.append(ks.layers.Dropout(0.2))
-    # fashion_classifier.append(ks.layers.Dense(num_classes, activation='softmax'))
 
-    fashion_class_model += [
-        ks.layers.Dropout(0.2),
-        ks.layers.Dense(40, activation='relu'),
-        ks.layers.Dropout(0.2),
-        ks.layers.Dense(num_classes, activation='softmax')
-    ]
+    fashion_class_model.add(ks.layers.Dropout(0.2))
+    fashion_class_model.add(ks.layers.Dense(40, activation='relu'))
+    fashion_class_model.add(ks.layers.Dropout(0.2))
+    fashion_class_model.add(ks.layers.Dense(num_classes, activation='softmax'))
+    
+
+
     return fashion_class_model
