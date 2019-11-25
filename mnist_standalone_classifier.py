4c29457c17e7d920f17e2ce418d8e45528010d44
Zaid <b4kxi96rpebp@opayq.com>
2019-11-25 04:02:18 -0500

    Improve classifiers

--- mnist_standalone_classifier.py
+++ mnist_standalone_classifier.py
@@ -5,6 +5,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 from keras.models import load_model
 import tensorflow as tf
@@ -14,32 +15,40 @@ import mnist_model
 model_path = 'data/model_mnist_classifier.h5'
 batch_size = 128
 num_classes = 10
-epochs = 200
+epochs = 100
 patience = 40
 learn_rate = 0.01
+
+img_rows, img_cols, img_channels = 28, 28, 1
+
+train_datagen = ImageDataGenerator(shear_range=0.2, zoom_range=0.2, rotation_range=10)
+valid_datagen = ImageDataGenerator(shear_range=0.2, zoom_range=0.2, rotation_range=10)
 
 # Create and build the model from layers, print model info
 
 model = ks.models.Sequential([
-    # # Convolutional Layer with 20 neurons and 3x3 receptive field and ReLU activation
-    # #       and using Batch Normalization and Dropout
-    # ks.layers.Conv2D(20, (3, 3), padding='same', use_bias=False,
-    #              input_shape=(28, 28, 1)),
-    # ks.layers.BatchNormalization(),
-    # ks.layers.Activation('relu'),
-    # ks.layers.Dropout(0.2),
-    # # Convolutional Layer with 10 neurons and 3x3 receptive field and ReLU activation
-    # #       and using Batch Normalization and Dropout
-    # ks.layers.Conv2D(10, (3, 3), padding='same', use_bias=False),
-    # ks.layers.BatchNormalization(),
-    # ks.layers.Activation('relu'),
-    # # Max Pooling layer of size 2x2
-    # ks.layers.MaxPooling2D(pool_size=(2, 2)),
-    # ks.layers.Dropout(0.2),
+    # The input layer
+    ks.layers.InputLayer(input_shape=(img_rows, img_cols, img_channels)),
+    # Convolutional Layer with 10 neurons and 3x3 receptive field and ReLU activation
+    #       and using Batch Normalization and Dropout
+    ks.layers.Conv2D(8, (3, 3), padding='same', use_bias=False),
+    ks.layers.BatchNormalization(),
+    ks.layers.Activation('relu'),
+    # Max Pooling layer of size 2x2
+    ks.layers.MaxPooling2D(pool_size=(2, 2)),
+    ks.layers.Dropout(0.2),
+    # Convolutional Layer with 10 neurons and 3x3 receptive field and ReLU activation
+    #       and using Batch Normalization and Dropout
+    ks.layers.Conv2D(8, (3, 3), padding='same', use_bias=False),
+    ks.layers.BatchNormalization(),
+    ks.layers.Activation('relu'),
+    # Max Pooling layer of size 2x2
+    ks.layers.MaxPooling2D(pool_size=(2, 2)),
+    ks.layers.Dropout(0.2),
     # Fully connected Layer with 100 neurons ReLU activation
     #       and using Batch Normalization and Dropout
     ks.layers.Flatten(),              # Flatten first
-    ks.layers.Dense(196, use_bias=False),
+    ks.layers.Dense(40, use_bias=False),
     ks.layers.BatchNormalization(),
     ks.layers.Activation('relu'),
     ks.layers.Dropout(0.2),
@@ -54,7 +63,7 @@ model.compile(
     loss=ks.losses.categorical_crossentropy,
     metrics=['accuracy']
 )
-# model.summary()
+model.summary()
 
 
 #%%
@@ -71,10 +80,17 @@ with h5py.File('data/mnist.hdf5', 'r') as data_file:
     x_test = np.array(data_file['x_test'])
     y_test = np.array(data_file['y_test'])
 
+    train_datagen.fit(lx_train)
+    valid_datagen.fit(lx_val)
+
+    train_generator = train_datagen.flow(lx_train, ly_train, batch_size=batch_size)
+    valid_generator = valid_datagen.flow(lx_val, ly_val, batch_size=batch_size)
+
     # Train model
-    model.fit(lx_train, ly_train,
-        validation_data=(lx_val, ly_val),
-        batch_size=batch_size,  shuffle='batch',
+    model.fit_generator(train_generator,
+        validation_data=valid_generator,
+        steps_per_epoch = 20, 
+        validation_steps = 5,
         epochs=epochs,
         verbose=1,
         callbacks=[
